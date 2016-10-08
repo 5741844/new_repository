07916c52bcac846259d32db7cae32a9e310cb5b1
redhuman <5741844@gmail.com>
2016-10-08 16:08:41 +0300

    Шаг 3 до функция render()

--- redhuman_is/the_redhuman_is/models.py
+++ redhuman_is/the_redhuman_is/models.py
@@ -3,32 +3,49 @@
 строка 17 в  паспорте #int_or_foreign = #сюда надо вставить условие, если переменная х == внутренний, то значение "внутренний", иначе "заграничный"" """
 
 from __future__ import unicode_literals
+
+import datetime
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+from django.utils import timezone
 
 # Create your models here.
 class Worker(models.Model):
+    input_date = models.DateTimeField('date published')
     last_name = models.CharField(max_length=100)
     name = models.CharField(max_length=100)
     patronymic = models.CharField(max_length=100)
     passport_number = models.CharField(max_length=9)
     birth_date = models.DateField()
+    mig_series = models.IntegerField()
+    mig_number = models.IntegerField()
+    m_date_of_issue = models.DateField()
+    m_date_of_exp = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+    def __str__(self):
+        return self.last_name
+
+    def was_born_recently(self):
+        return self.birth_date >= (timezone.now() - datetime.timedelta(days=1))
 
 class WorkerPassport(models.Model):
-    worker_id = models.ForeignKey(Worker, on_delete=models.CASCADE)
+    workers_id = models.ForeignKey(Worker, on_delete=models.CASCADE)
     another_passport_number = models.CharField(max_length=9)
     date_of_issue = models.DateField()
     date_of_exp = models.DateField()
     issued_by = models.CharField(max_length=100)
 
 class WorkerRegistration(models.Model):
-    worker_id = models.ForeignKey(Worker, on_delete=models.CASCADE)
+    workers_id = models.ForeignKey(Worker, on_delete=models.CASCADE)
     city = models.CharField(max_length=100)
     street = models.CharField(max_length=100)
     house_number = models.CharField(max_length=100)
     building_number = models.CharField(max_length=100)
     appt_number = models.CharField(max_length=100)
 
-class WorkerMigrationCard(models.Model):
-    worker_id = models.ForeignKey(Worker, on_delete=models.CASCADE)
-    series = models.IntegerField()
-    number = models.IntegerField()
+class TelNumber(models.Model):
+    workers_id = models.ForeignKey(Worker, on_delete=models.CASCADE)
+    tel_number = models.IntegerField(default=89)
--- redhuman_is/the_redhuman_is/views.py
+++ redhuman_is/the_redhuman_is/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Worker
+
+from django.template import RequestContext, loader
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello world.")
+    latest_worker_list = Worker.objects.order_by('-input_date')[:5]
+    template = loader.get_template('the_redhuman_is/index.html')
+    context = RequestContext(request, {
+        'latest_worker_list': latest_worker_list,
+    })
+    return HttpResponse(template.render(context))
+
+def detail(request, worker_id):
+    return HttpResponse("You're looking at worker %s." % worker_id)
+
+def results(request, worker_id):
+    response = "You're looking at the results of worker %s."
+    return HttpResponse(response % worker_id)
+
+def vote(request, worker_id):
+    return HttpResponse("You're voting on worker %s." % worker_id)
